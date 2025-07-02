96580d87138da810ac12529bb32e99af8580c35c
okoro91 <mi.fracri91@gmail.com>
2025-07-02 11:12:06 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Header from "./components/Header";
+import Main from "./components/Main";
+import Footer from "./components/Footer";
+import "./App.css";
+
+import React from "react";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+};
+
+export default App;
---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>make your best cook</p>
+      <p>copyrite chef O</p>
+    </footer>
+  );
+};
+
+export default Footer;
