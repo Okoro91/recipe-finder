1ed4f9f01c79253503a2fed142a8d5cdb1799785
okoro91 <mi.fracri91@gmail.com>
2025-07-09 23:45:46 +0100

    used reactmarkdown to format AI response

--- ai.js
+++ ai.js
@@ -1,7 +1,7 @@
 import { InferenceClient } from "@huggingface/inference";
 
 const SYSTEM_PROMPT = `
-You are an assistant that receives a list of ingredients that a user has and suggests a recipe they could make...
+You are an assistant that receives a list of ingredients that I have and suggests a Nigerian or African recipe I could make... : always start with chef Okoro recommends...
 `;
 
 const client = new InferenceClient(import.meta.env.VITE_REACT_APP_HF_TOKEN);
@@ -17,7 +17,7 @@ export async function getGeneratedRecipe(ingredientsArr) {
         { role: "system", content: SYSTEM_PROMPT },
         {
           role: "user",
-          content: `I have ${ingredientsString}. Please give me a recipe you'd recommend I make!`,
+          content: `I have ${ingredientsString}. Please give me an African recipe you'd recommend I make!`,
         },
       ],
       max_tokens: 1024,
--- src/components/items/Recipe.jsx
+++ src/components/items/Recipe.jsx
@@ -3,6 +3,7 @@ import ReactMarkdown from "react-markdown";
 const Recipe = (props) => {
   return (
     <section>
+      <h1>Chef Okoro Recommends</h1>
       <ReactMarkdown>{props.showRecipe}</ReactMarkdown>
     </section>
   );
